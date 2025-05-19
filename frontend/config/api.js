50c4ab8c71376dbedfb8dafbdd0b45fa1c8f5d3e
Tokyoooo20 <152415717+Tokyoooo20@users.noreply.github.com>
2025-05-19 15:15:13 +0800

    chnage the api

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,2 +1,1 @@
-// Update this IP address when connecting to a different network
-export const API_URL = 'http://192.168.254.179:5000'; 
+export const API_URL = 'https://mediasphere-backend.onrender.com';
